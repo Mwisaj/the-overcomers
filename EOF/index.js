c4eb111fe7721710eaa5f2e89966962e86fa44f5
Mwisaj <141968247+Mwisaj@users.noreply.github.com>
2025-01-04 23:20:52 +0200

    Add files via upload
    The Envisionally Overcomers Foundation

---
+++ EOF/index.js
@@ -0,0 +1,13 @@
+$("document").ready(function(){
+$(".menu-icon").click(function(){
+$("#menu").slideToggle()
+});
+});
+
+function paragraph(){
+    document.getElementById("para1").innerHTML="<h1>Why was this foundation formed?</h1>This is a Non-govermental Organisation with the aim to help it's members and Non-members in different communinties of our country. as the Envisionary overcomers foundation we more passianate about education, the  future of the youths  and involving our selves in diferent activities to support young talents";
+        $(body).css("background-color","black");
+    };
+function darkMode(){
+    document.getElementsByTagName("body").style.background-Color-"black";
+}
